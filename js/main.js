0b0e7c5e8fc1cc3bb546b183adb2027690ca232b
muslim0602 <muslimturaboyev0602@gmail.com>
2022-08-04 19:50:04 +0500

    Add dataset attribute

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 const elSiteHeaderCartLink = document.querySelector(".js-site-header-cart-link");
 const elSiteHeaderCartModal = document.querySelector(".site-header__cart-modal");
 
-if (elSiteHeaderCartLink){
-elSiteHeaderCartLink.addEventListener("click", function (evt){
-  evt.preventDefault();
+if (elSiteHeaderCartLink) {
+  elSiteHeaderCartLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
 
-  elSiteHeaderCartModal.classList.toggle("site-header__cart-modal--open");
-});
+    elSiteHeaderCartModal.classList.toggle("site-header__cart-modal--open");
+  });
 }
 
-
+const elPicturePrime = document.querySelector(".picture__prime");
 const elsPicturesPrimeButton = document.querySelectorAll(".js-pictures-prime-button");
 const elsPicturesItem = document.querySelectorAll(".pictures__item");
 
 elsPicturesPrimeButton.forEach(function (elButton) {
 
-elButton.addEventListener("click", function (){
-  elsPicturesItem.forEach(function (elsPicturesItem){
-    elsPicturesItem.classList.remove("pictures__item--active");
+  elButton.addEventListener("click", function () {
+    elsPicturesItem.forEach(function (elsPicturesItem) {
+      elsPicturesItem.classList.remove("pictures__item--active");
+
+    });
+
+    elButton.parentElement.classList.add("pictures__item--active");
+
+    elPicturePrime.src = elButton.dataset.elPicturesBog;
+    elPicturePrime.srcset ='${elButton.dataset.imgPicturesBog} 1x, ${elButton.dataset.imgPicturesRetina} 2x';
 
   });
-
-  elButton.parentElement.classList.add("pictures__item--active");
-
-});
 
 });
