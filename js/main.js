b58f3bf82e8fc1fafcda072bbff6d1a91f43514c
muslim0602 <muslimturaboyev0602@gmail.com>
2022-08-06 12:42:18 +0500

    Add product quantity styles JS

--- js/main.js
+++ js/main.js
@@ -52,3 +52,56 @@ if (elLightboxClose) {
   });
 
 }
+
+// LIGHTBOX CONTROL
+
+const elLightBoxControlPrev = elLightBox.querySelector(".js-lightbox-control-prev");
+const elLightBoxControlNext = elLightBox.querySelector(".js-lightbox-control-next");
+
+if (elLightBoxControlPrev) {
+  elLightBoxControlNext.addEventListener("click", function name(params) {
+    const elLightBoxActive = elLightBox.querySelector(".pictures__item--active");
+    elLightBoxActive.classList.remove("pictures__item--active");
+
+    let elNextActiveItem;
+
+    if (elLightBoxActive.nextElementSibling === null) {
+      elNextActiveItem = elsPicturesItem[0];
+    }
+    else {
+      elNextActiveItem = elLightBoxActive.nextElementSibling;
+
+    }
+
+    elNextActiveItem.classList.add("pictures__item--active");
+
+    elPicturePrime.src = elButton.dataset.elPicturesBog;
+    elPicturePrime.srcset = '${elButton.dataset.imgPicturesBog} 1x, ${elButton.dataset.imgPicturesRetina} 2x';
+  });
+
+}
+
+// PRODUCT COUNT
+
+const elProductQuantityMinus = document.querySelector(".js-quantity-minus");
+const elProductQuantityPlus  = document.querySelector(".js-quantity-plus");
+const elProductSum = document.querySelector(".sum");
+
+if (elProductQuantityPlus) {
+  elProductQuantityPlus.addEventListener("click", function () {
+elProductSum.textContent =parseInt(elProductSum.textContent, 10) + 1;
+  });
+
+}
+
+if (elProductQuantityMinus) {
+  elProductQuantityMinus.addEventListener("click", function () {
+    const qty = parseInt(elProductSum.textContent, 10)
+    if (qty > 0) {
+
+    elProductSum.textContent =qty - 1;
+    }
+
+  });
+
+}
